564a826d1c2a5945ffca590b4e858b6c59417979
Joonmo <yunedoopea610@gmail.com>
2019-10-16 00:04:05 +0900

    Fix category type
    카테고리랑 메뉴가 따로놀음. 둘에게 의존성을 부여

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,6 @@ import random
 
 app = Flask(__name__)
 
-categories = ['한식', '일식', '중식', '양식']
 menus = {'한식': ['국밥', '갈비탕', '냉면', '치킨', '해장국', '순두부찌개', '김치찌개', '부대찌개', '떡볶이', '비빔밥', '된장', '감자탕', '칼국수', '설렁탕', '보쌈', '김원준'],
          '일식': ['우동', '규동', '소바', '라멘', '카레', '돈까스', '오므라이스', '햄버그 스테이크'],
          '중식': ['짜장면', '짬뽕', '볶음밥', '마라탕', '마라샹궈', '기스면', '깐풍기', '탕수육', '소룡포', '우육면'],
@@ -13,7 +12,7 @@ menus = {'한식': ['국밥', '갈비탕', '냉면', '치킨', '해장국', '순
 def index():
     category = request.form['text']
 
-    if not category or category not in categories:
+    if not category or category not in menus.keys():
         category = random.choice(categories)
 
     food = random.choice(menus[category])
