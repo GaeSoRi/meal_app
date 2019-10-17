668067b75fa0d417c2674aef878143196b2b18e4
Namkyu An <skarb7746@gmail.com>
2019-10-17 22:02:31 +0900

    data.json을 불러오도록 변경

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,18 @@
 from flask import Flask, request
 import random
+import json
 
 app = Flask(__name__)
-
-menus = {'한식': ['국밥', '갈비탕', '냉면', '치킨', '해장국', '순두부찌개', '김치찌개', '부대찌개', '떡볶이', '비빔밥', '된장', '감자탕', '칼국수', '설렁탕', '보쌈', '김원준'],
-         '일식': ['우동', '규동', '소바', '라멘', '카레', '돈까스', '오므라이스', '햄버그 스테이크'],
-         '중식': ['짜장면', '짬뽕', '볶음밥', '마라탕', '마라샹궈', '기스면', '깐풍기', '탕수육', '소룡포', '우육면'],
-         '양식': ['돈까스', '피자', '햄버거', '스파게티', '타코']}
 
 @app.route('/', methods=['POST'])
 def index():
     category = request.form['text']
 
-    if category not in menus.keys():
+    with open('data.json') as json_data:
+        menus = json.load(json_data)
+        categories = menus.keys()
+
+    if category not in categories:
         category = random.choice(categories)
 
     food = random.choice(menus[category])
@@ -25,4 +25,4 @@ def index():
     return res
 
 if __name__ == "__main__":
-    app.run(port=1212)
+    app.run(port=1212, debug=True)
