b841e8d9d198890987819a6f3bb646a1f28065a5
Namkyu An <skarb7746@gmail.com>
2019-10-20 22:34:16 +0900

    연준모 요구사항 진짜 반영

--- tests/test_tmp.py
+++ tests/test_tmp.py
@@ -6,5 +6,9 @@ def test_get_food():
     assert res.status_code == 200
 
 def test_get_food_text_value():
-    res = app.post('/', data={'text': ''}, content_type='multipart/form-data')
+    res = app.post('/', data={'text': '한식'}, content_type='multipart/form-data')
+    assert res.status_code == 200
+
+def test_get_food_text_non_exist_value():
+    res = app.post('/', data={'text': '가나다'}, content_type='multipart/form-data')
     assert res.status_code == 200
