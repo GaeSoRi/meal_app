0922b09649a3ee4facb2e503b6cbacb5f98f4630
Namkyu An <skarb7746@gmail.com>
2019-10-20 22:28:30 +0900

    연준모 요구사항 반영

--- tests/test_tmp.py
+++ tests/test_tmp.py
@@ -4,3 +4,7 @@ app = chatbot.app.test_client()
 def test_get_food():
     res = app.post('/', data={'text': ''}, content_type='multipart/form-data')
     assert res.status_code == 200
+
+def test_get_food_text_value():
+    res = app.post('/', data={'text': ''}, content_type='multipart/form-data')
+    assert res.status_code == 200
